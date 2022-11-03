896a37bb464d84e75f578bdb2a066067cbfe8587
Isaac Alter <i_alter@yahoo.com>
2022-11-03 14:11:30 -0600

    Adds code to assignment

--- hashes_1.rb
+++ hashes_1.rb
@@ -1,30 +1,33 @@
 
-Activity:
+#Activity:
 
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
 # file by entering the following command in your Terminal: 
 # `ruby hashes_1.rb`
 
-foods = {"apples": 23, "grapes": 507, "eggs": 48}
-p zoo
+foods = {
+  apples: 23, 
+  grapes: 507, 
+  eggs: 48
+}
 
 # Write code that prints all of the 'keys' of the foods variable 
 # you created above:
-# YOUR CODE HERE
+p foods.keys
 
 # Write code that prints all of the 'values' of the foods variable 
 # you created above:
-# YOUR CODE HERE
+p foods.values
 
 # Write code that prints the value of the second food of the foods variable 
 # you created above:
-# YOUR CODE HERE
+p foods[:grapes]
 
 # Write code that adds a food to the foods hash. 
 # Then, print the updated hash:
-# YOUR CODE HERE
-
+foods[:beef] = 36
+p foods
 
 #-------------------
 # Part 2: Email
@@ -37,16 +40,24 @@ p zoo
 # Declare a variable that stores hash. Each key should be an attribute of an email and each
 # value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
 
-# Write code that prints your email hash to the terminal.
+email = {
+  recipient: "Bob Doe",
+  sender: "Isaac Alter",
+  subject: "Where's my money?",
+  message: "Hey, it's been about a month now, and you still haven't paid me back. Do I have to get a third party involved?",
+  sent: true
+}
 
+# Write code that prints your email hash to the terminal.
+p email
 
 # Write code that prints all of the 'keys' of the email hash 
 # you created above:
-# YOUR CODE HERE
+p email.keys
 
 # Write code that prints all of the 'values' of the email hash 
 # you created above:
-# YOUR CODE HERE
+p email.values
 
 
 #-------------------
@@ -97,3 +108,29 @@ p posts[0]
 # YOU DO: Create an array of at least 3 EMAIL Hashes, using the same 
 # key-value pairs you used in your email Hash above.
 # Then, print the email Array to the Terminal.
+
+email_hashes = [
+  {
+    recipient: "Tarzan",
+    sender: "Jane",
+    subject: "New Laptop",
+    message: "Are you enjoying the computer? It was really hard to get it shipped out to the jungle.",
+    sent: true
+  },
+  {
+    recipient: "Jane",
+    sender: "Tarzan",
+    subject: "Re:New Laptop",
+    message: "sdwjhdwihdbvfvebuh dei ujfgdwgfue grretjbuicdef hgriuyf fy gr fruhfr iugrr",
+    sent: true
+  },
+  {
+    recipient: "Tarzan",
+    sender: "Jane",
+    subject: "Re:Re:New Laptop",
+    message: "Why did I buy this for you?",
+    sent: false
+  }
+]
+
+p email_hashes
